add7bce56c1bda0b78982286b3810d0877febf0c
Jean Tan Li <chines1998@hotmail.com>
2019-05-18 17:56:31 -0300

    MenuPause

---
+++ WelcomeToTheDevil/Assets/Scripts/MenuPause.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;
+public class MenuPause : MonoBehaviour
+{
+    public static bool paused = false;
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+     if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(paused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        paused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        paused = true;
+
+    }
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("MenuInicial");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
