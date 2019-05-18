7fe5db38cc89c8382c7d8983d666e7a4a76a160a
Waldomiro Barbosa Romão <devwaldomiroromao@gmail.com>
2019-05-18 16:05:02 -0300

    Adição de pulo

--- WelcomeToTheDevil/Assets/Scripts/PlayerControl.cs
+++ WelcomeToTheDevil/Assets/Scripts/PlayerControl.cs
@@ -5,18 +5,33 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     Rigidbody2D rb;
+    public LayerMask isGround;
+    public Transform groundCheck;
+
+    bool jumpToGo;
 
     public float fallMultiplier = 2.5f;
     public float lowJumpMultiplier = 2f;
+    public float checkRadius;
+
+    [Range(1,10)]
+    public float jumpVelocity;
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update() {
+
+        jumpToGo = Physics2D.OverlapCircle(groundCheck.position, checkRadius, isGround);
+
+        if (Input.GetKeyDown("up") && jumpToGo == true){
+            rb.velocity = Vector2.up * jumpVelocity;
+            jumpToGo = false;
+        }
         if (rb.velocity.y < 0) {
             rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
-        } else if (rb.velocity.y > 0 && !Input.GetButton("Jump")) {
+        } else if (rb.velocity.y > 0 && !Input.GetKeyDown("up")) {
             rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
     }
